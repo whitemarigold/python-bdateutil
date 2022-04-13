52feab0d993bf81238057396a7d30a732f7c4fa5
Tyler Copple <j.tylercopple@gmail.com>
2022-04-13 11:39:26 -0400

    Updating parser to handle stream types.

--- bdateutil/parser.py
+++ bdateutil/parser.py
@@ -1,15 +1,15 @@
-#  bdateutil
-#  -----------
+#  python-bdateutil
+#  ----------------
 #  Adds business day logic and improved data type flexibility to
 #  python-dateutil. 100% backwards compatible with python-dateutil,
 #  simply replace dateutil imports with bdateutil.
 #
 #  Author:  ryanss <ryanssdev@icloud.com>
-#  Website: https://github.com/ryanss/bdateutil
+#  Website: https://github.com/ryanss/python-bdateutil
 #  License: MIT (see LICENSE file)
 
 
-from datetime import date, datetime
+from datetime import date, datetime, time, timedelta
 
 from dateutil.parser import *
 from dateutil.parser import parser
@@ -17,6 +17,8 @@ import six
 
 
 def parse(timestr, parserinfo=None, **kwargs):
+    if getattr(timestr, "read", False):
+        timestr = timestr.read()
     if isinstance(timestr, six.binary_type):
         timestr = timestr.decode()
     if isinstance(timestr, six.string_types):
@@ -30,6 +32,8 @@ def parse(timestr, parserinfo=None, **kwargs):
     elif isinstance(timestr, int) or isinstance(timestr, float):
         ret = datetime.fromtimestamp(timestr)
     elif isinstance(timestr, datetime) or isinstance(timestr, date):
+        ret = timestr
+    elif isinstance(timestr, time) or isinstance(timestr, timedelta):
         ret = timestr
     else:
         raise TypeError("Can't convert %s to date." % type(timestr))
